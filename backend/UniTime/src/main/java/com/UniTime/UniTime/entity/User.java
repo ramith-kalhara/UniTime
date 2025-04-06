1c91648d97fe49cb30c42f06249f74e43aa2e8bf
DanukBiyanwila <151656079+DanukBiyanwila@users.noreply.github.com>
2025-04-06 16:05:43 +0530

    a
    a

--- backend/UniTime/src/main/java/com/UniTime/UniTime/entity/User.java
+++ backend/UniTime/src/main/java/com/UniTime/UniTime/entity/User.java
@@ -2,14 +2,14 @@ package com.UniTime.UniTime.entity;
 
 import com.UniTime.UniTime.dto.UserDto;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.modelmapper.ModelMapper;
 
-@Setter
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -18,34 +18,25 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
-    @Column(name = "firstName", nullable = false, length = 100)
     private String firstName;
-
-    @Column(name = "lastName", nullable = false, length = 100)
     private String lastName;
-
-    @Column(name = "tpNum", nullable = false)
     private String tpNum;
-
-    @Column(name = "password", nullable = false)
     private String password;
-
-    @Column(name = "bookRoomId", nullable = false)
     private String bookRoomId;
-
-    @Column(name = "courseId", nullable = false)
     private String courseId;
-
-    @Column(name = "email", nullable = false)
     private String email;
-
-    @Column(name = "moduleId", nullable = false)
     private String moduleId;
 
-    // Convert the User Entity to UserDto
+    @ManyToMany
+    @JoinTable(
+            name = "user_schedule",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "schedule_id")
+    )
+    private Set<Schedule> schedules = new HashSet<>();
+
     public UserDto toDto(ModelMapper mapper) {
         return mapper.map(this, UserDto.class);
     }
