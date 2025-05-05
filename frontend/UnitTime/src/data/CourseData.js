13d5b699e13ac562bf5b8b2fd0008d07a0946211
ramith-kalhara <67057497+ramith-kalhara@users.noreply.github.com>
2025-05-05 21:20:37 +0530

    UserVote and professor relations (half)

--- frontend/UnitTime/src/data/CourseData.js
+++ frontend/UnitTime/src/data/CourseData.js
@@ -1,5 +1,6 @@
 
 import blog1 from "../assets/user/img/blog-1.jpg"
+import blog2 from "../assets/user/img/blog-2.jpg"
 const CourseData = [
   {
     id: 0,
@@ -15,7 +16,7 @@ const CourseData = [
 
   {
     id: 2,
-    img: blog1,
+    img: blog2,
     moduleName: "Data ",
     description: "Sed kasd sea sed at elitr sed ipsum justo, sit nonumy diam eirmod, duo et sed sit eirmod kasd clita tempor dolor stet lorem. Tempor ipsum justo amet stet...",
     department: "Data science",
@@ -45,7 +46,19 @@ const CourseData = [
     creadit: "4",
     tags: ["Project Management", "Software Engineering", "Teamwork", "Agile", "Scrum"],
      descriptionLong: " Sadipscing labore amet rebum est et justo gubergren. Et eirmod ipsum sit diam ut magna lorem. Nonumy vero labore lorem sanctusrebum et lorem magna kasd, stet amet magna accusam consetetureirmod. Kasd accusam sit ipsum sadipscing et at at sanctus et.Ipsum sit gubergren dolores et, consetetur justo invidunt at etaliquyam ut et vero clita. Diam sea sea no sed dolores diam  nonumy, gubergren sit stet no diam kasd vero."
-  }
+  },
+  {
+    id: 4,
+    img: blog1,
+    moduleName: "BMIT",
+    description: "Sed kasd sea sed at elitr sed ipsum justo, sit nonumy diam eirmod, duo et sed sit eirmod kasd clita tempor dolor stet lorem. Tempor ipsum justo amet stet...",
+    department: "civil engeenire",
+    courseCode: "IT2030",
+    creadit: "4",
+    tags: ["Project Management", "Software Engineering", "Teamwork", "Agile", "Scrum"],
+     descriptionLong: " Sadipscing labore amet rebum est et justo gubergren. Et eirmod ipsum sit diam ut magna lorem. Nonumy vero labore lorem sanctusrebum et lorem magna kasd, stet amet magna accusam consetetureirmod. Kasd accusam sit ipsum sadipscing et at at sanctus et.Ipsum sit gubergren dolores et, consetetur justo invidunt at etaliquyam ut et vero clita. Diam sea sea no sed dolores diam  nonumy, gubergren sit stet no diam kasd vero."
+  },
+  
 
 
 ];
